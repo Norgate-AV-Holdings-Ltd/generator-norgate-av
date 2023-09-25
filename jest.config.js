752a340500123f82b54b19cf9e7c60ea0d3f15bb
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2023-09-25 03:09:45 +0100

    chore: remove eslist and setup vitest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    verbose: true,
-    testEnvironment: "node",
-    testPathIgnorePatterns: [
-        "node_modules",
-        ".history",
-        "generators/app/templates/*",
-    ],
-};

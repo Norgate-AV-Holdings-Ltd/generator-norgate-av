b38026b55806f2cbc876af0aa823ebc283a467e7
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-02-10 19:40:05 +0000

    refactor: further updates

--- tests/python.test.ts
+++ tests/python.test.ts
@@ -9,75 +9,96 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const generator = path.resolve(__dirname, "../dist/generators/app");
 
 describe("generator-norgate-av:app", () => {
-    describe("python:skip-install", () => {
-        let result: RunResult<AppGenerator>;
+    describe.each([
+        {
+            type: "python",
+            name: "test",
+            description: "test-description",
+            git: false,
+            openWith: "skip",
+        },
+        {
+            type: "py",
+            name: "test",
+            description: "test-description",
+            git: true,
+            openWith: "skip",
+        },
+    ])(
+        'python using type "$type" and git "$git"',
+        ({ type, name, description, git, openWith }) => {
+            let result: RunResult<AppGenerator>;
 
-        const name = "test";
-        const description = "test-description";
+            beforeAll(async () => {
+                result = await helpers
+                    .create<AppGenerator>(generator)
+                    .withAnswers({
+                        type,
+                        name,
+                        description,
+                        git,
+                        openWith,
+                    });
 
-        beforeAll(async () => {
-            result = await helpers.create<AppGenerator>(generator).withAnswers({
-                type: "python",
-                name,
-                description,
-                git: false,
-                openWith: "skip",
+                process.chdir(name);
             });
 
-            process.chdir(name);
-        });
+            afterAll(() => {
+                result?.cleanup();
+            });
 
-        afterAll(() => {
-            result?.cleanup();
-        });
+            it(`should create a directory named '${name}' and CD into it`, () => {
+                assert.equal(path.basename(process.cwd()), name);
+            });
 
-        it(`should create a directory named '${name}' and CD into it`, () => {
-            assert.equal(path.basename(process.cwd()), name);
-        });
+            it("should create the correct files", () => {
+                assert.file([
+                    ".github/workflows/main.yml",
+                    ".github/dependabot.yml",
+                    ".vscode/extensions.json",
+                    ".vscode/settings.json",
+                    "src/main.py",
+                    ".editorconfig",
+                    ".gitattributes",
+                    ".gitignore",
+                    "CONTRIBUTING.md",
+                    "CHANGELOG.md",
+                    "GitVersion.yml",
+                    "LICENSE",
+                    "README.md",
+                ]);
+            });
 
-        it("should create the correct files", () => {
-            assert.file([
-                ".github/workflows/main.yml",
-                ".github/dependabot.yml",
-                ".vscode/extensions.json",
-                ".vscode/settings.json",
-                "src/main.py",
-                ".editorconfig",
-                ".gitattributes",
-                ".gitignore",
-                "CONTRIBUTING.md",
-                "CHANGELOG.md",
-                "GitVersion.yml",
-                "LICENSE",
-                "README.md",
-            ]);
-        });
+            it("should create the correct README.md", () => {
+                assert.fileContent("README.md", `# ${name}`);
+                assert.fileContent(
+                    "README.md",
+                    `This is the README for your project "${name}". After writing up a brief description, we recommend including the following sections.`,
+                );
+            });
 
-        it("should create the correct README.md", () => {
-            assert.fileContent("README.md", `# ${name}`);
-            assert.fileContent(
-                "README.md",
-                `This is the README for your project "${name}". After writing up a brief description, we recommend including the following sections.`,
-            );
-        });
+            it("should create the correct LICENSE", () => {
+                assert.fileContent("LICENSE", "The MIT License (MIT)");
+                assert.fileContent(
+                    "LICENSE",
+                    `Copyright (c) ${new Date().getFullYear()}`,
+                );
+            });
 
-        it("should create the correct LICENSE", () => {
-            assert.fileContent("LICENSE", "The MIT License (MIT)");
-            assert.fileContent(
-                "LICENSE",
-                `Copyright (c) ${new Date().getFullYear()}`,
-            );
-        });
+            it("should create the correct CHANGELOG.md", () => {
+                assert.fileContent(
+                    "CHANGELOG.md",
+                    `All notable changes to the "${name}" project will be documented in this file.`,
+                );
+            });
 
-        it("should create the correct CHANGELOG.md", () => {
-            assert.fileContent(
-                "CHANGELOG.md",
-                `All notable changes to the "${name}" project will be documented in this file.`,
-            );
-        });
+            it("should create a git repository if git is true", () => {
+                git ? assert.file(".git") : assert.noFile(".git");
+            });
 
-        it("should always pass", () => {
-            expect(1).toEqual(1);
-        });
-    });
+            it("should always pass", () => {
+                expect(1).toEqual(1);
+            });
+        },
+    );
 });
--- tests/simpl.test.ts
+++ tests/simpl.test.ts
@@ -21,144 +21,185 @@ const { devDependencies } = node.packageJson;
 const engine = node.getNodeEngine().split(".")[0];
 
 describe("generator-norgate-av:app", () => {
-    describe("simpl:skip-install", () => {
-        let result: RunResult<AppGenerator>;
+    describe.each([
+        {
+            type: "simpl",
+            name: "test",
+            description: "test-description",
+            git: false,
+            pkg: "pnpm",
+            openWith: "skip",
+        },
+        {
+            type: "crestron-simpl",
+            name: "test",
+            description: "test-description",
+            git: true,
+            pkg: "yarn",
+            openWith: "skip",
+        },
+        {
+            type: "simpl",
+            name: "test",
+            description: "test-description",
+            git: true,
+            pkg: "npm",
+            openWith: "skip",
+        },
+    ])(
+        'crestron-simpl with type $type, package manager $pkg, and git "$git"',
+        ({ type, name, description, git, pkg, openWith }) => {
+            let result: RunResult<AppGenerator>;
 
-        const name = "test";
-        const description = "test-description";
-        const pkg = "pnpm";
+            beforeAll(async () => {
+                result = await helpers
+                    .create<AppGenerator>(generator)
+                    .withAnswers({
+                        type,
+                        name,
+                        description,
+                        git,
+                        pkg,
+                        openWith,
+                    });
 
-        beforeAll(async () => {
-            result = await helpers.create<AppGenerator>(generator).withAnswers({
-                type: "simpl",
-                name,
-                description,
-                git: false,
-                pkg,
-                openWith: "skip",
+                process.chdir(name);
             });
 
-            process.chdir(name);
-        });
-
-        afterAll(() => {
-            result?.cleanup();
-        });
-
-        it(`should create a directory named '${name}' and CD into it`, () => {
-            assert.equal(path.basename(process.cwd()), name);
-        });
-
-        it("should create the correct files", () => {
-            assert.file([
-                ".github/workflows/main.yml",
-                ".github/dependabot.yml",
-                ".husky/commit-msg",
-                ".husky/pre-commit",
-                ".vscode/extensions.json",
-                ".vscode/settings.json",
-                "config/config.txt",
-                "docs/.gitkeep",
-                "src/.gitkeep",
-                "ui/.gitkeep",
-                ".all-contributorsrc",
-                ".changelogrc.json",
-                ".commitlintrc.json",
-                ".czrc",
-                ".editorconfig",
-                ".gitattributes",
-                ".gitignore",
-                ".lintstagedrc.json",
-                ".npmignore",
-                ".npmrc",
-                ".nvmrc",
-                ".prettierignore",
-                ".prettierrc.json",
-                "CHANGELOG.md",
-                "CONTRIBUTING.md",
-                "GitVersion.yml",
-                "LICENSE",
-                "package.json",
-                "README.md",
-            ]);
-        });
-
-        it.skip("should create the correct package.json", () => {
-            assert.jsonFileContent("package.json", {
-                name,
-                description,
-                engines: {
-                    node: `>=${engine}`,
-                },
-                devDependencies: {
-                    "@commitlint/cli": devDependencies!["@commitlint/cli"],
-                    "@commitlint/config-conventional":
-                        devDependencies!["@commitlint/config-conventional"],
-                    "all-contributors-cli":
-                        devDependencies!["all-contributors-cli"],
-                    commitizen: devDependencies!.commitizen,
-                    "cz-conventional-changelog":
-                        devDependencies!["cz-conventional-changelog"],
-                    doctoc: devDependencies!.doctoc,
-                    husky: devDependencies!.husky,
-                    "lint-staged": devDependencies!["lint-staged"],
-                    prettier: devDependencies!.prettier,
-                },
+            afterAll(() => {
+                result?.cleanup();
             });
-        });
 
-        it("should create the correct README.md", () => {
-            assert.fileContent("README.md", `# ${name}`);
-            assert.fileContent(
-                "README.md",
-                `This is the README for your project "${name}". After writing up a brief description, we recommend including the following sections.`,
-            );
-        });
+            it(`should create a directory named '${name}' and CD into it`, () => {
+                assert.equal(path.basename(process.cwd()), name);
+            });
 
-        it("should create the correct .nvmrc", () => {
-            assert.fileContent(".nvmrc", node.getNodeEngine());
-        });
+            it("should create the correct files", () => {
+                assert.file([
+                    ".github/workflows/main.yml",
+                    ".github/dependabot.yml",
+                    ".husky/commit-msg",
+                    ".husky/pre-commit",
+                    ".vscode/extensions.json",
+                    ".vscode/settings.json",
+                    "config/config.txt",
+                    "docs/.gitkeep",
+                    "src/.gitkeep",
+                    "ui/.gitkeep",
+                    ".all-contributorsrc",
+                    ".changelogrc.json",
+                    ".commitlintrc.json",
+                    ".czrc",
+                    ".editorconfig",
+                    ".gitattributes",
+                    ".gitignore",
+                    ".lintstagedrc.json",
+                    ".npmignore",
+                    ".npmrc",
+                    ".nvmrc",
+                    ".prettierignore",
+                    ".prettierrc.json",
+                    "CHANGELOG.md",
+                    "CONTRIBUTING.md",
+                    "GitVersion.yml",
+                    "LICENSE",
+                    "package.json",
+                    "README.md",
+                ]);
+            });
 
-        it("should create the correct LICENSE", () => {
-            assert.fileContent("LICENSE", "The MIT License (MIT)");
-            assert.fileContent(
-                "LICENSE",
-                `Copyright (c) ${new Date().getFullYear()}`,
-            );
-        });
+            it.skip("should create the correct package.json", () => {
+                assert.jsonFileContent("package.json", {
+                    name,
+                    description,
+                    engines: {
+                        node: `>=${engine}`,
+                    },
+                    devDependencies: {
+                        "@commitlint/cli": devDependencies!["@commitlint/cli"],
+                        "@commitlint/config-conventional":
+                            devDependencies!["@commitlint/config-conventional"],
+                        "all-contributors-cli":
+                            devDependencies!["all-contributors-cli"],
+                        commitizen: devDependencies!.commitizen,
+                        "cz-conventional-changelog":
+                            devDependencies!["cz-conventional-changelog"],
+                        doctoc: devDependencies!.doctoc,
+                        husky: devDependencies!.husky,
+                        "lint-staged": devDependencies!["lint-staged"],
+                        prettier: devDependencies!.prettier,
+                    },
+                });
+            });
 
-        it("should create the correct CONTRIBUTING.md", () => {
-            assert.fileContent("CONTRIBUTING.md", `/${name}/issues/new/choose`);
-            assert.fileContent("CONTRIBUTING.md", `/${name}.git`);
-            assert.fileContent("CONTRIBUTING.md", `cd ${name}`);
-            assert.fileContent("CONTRIBUTING.md", `${pkg} install`);
-            assert.fileContent("CONTRIBUTING.md", `${pkg} contrib:add`);
-        });
+            it("should create the correct README.md", () => {
+                assert.fileContent("README.md", `# ${name}`);
+                assert.fileContent(
+                    "README.md",
+                    `This is the README for your project "${name}". After writing up a brief description, we recommend including the following sections.`,
+                );
+            });
 
-        it("should create the correct CHANGELOG.md", () => {
-            assert.fileContent(
-                "CHANGELOG.md",
-                `All notable changes to the "${name}" project will be documented in this file.`,
-            );
-        });
+            it("should create the correct .nvmrc", () => {
+                assert.fileContent(".nvmrc", node.getNodeEngine());
+            });
 
-        it("should create the correct .all-contributorsrc", () => {
-            assert.fileContent(
-                ".all-contributorsrc",
-                `"projectName": "${name}"`,
-            );
-        });
+            it("should create the correct LICENSE", () => {
+                assert.fileContent("LICENSE", "The MIT License (MIT)");
+                assert.fileContent(
+                    "LICENSE",
+                    `Copyright (c) ${new Date().getFullYear()}`,
+                );
+            });
 
-        it("should create the correct husky git hooks", () => {
-            assert.fileContent(
-                ".husky/commit-msg",
-                `${pkg} commitlint --edit $1`,
-            );
-            assert.fileContent(".husky/pre-commit", `${pkg} lint-staged`);
-        });
+            it("should create the correct CONTRIBUTING.md", () => {
+                assert.fileContent(
+                    "CONTRIBUTING.md",
+                    `/${name}/issues/new/choose`,
+                );
+                assert.fileContent("CONTRIBUTING.md", `/${name}.git`);
+                assert.fileContent("CONTRIBUTING.md", `cd ${name}`);
+                assert.fileContent("CONTRIBUTING.md", `${pkg} install`);
+                assert.fileContent("CONTRIBUTING.md", `${pkg} contrib:add`);
+                assert.fileContent(
+                    "CONTRIBUTING.md",
+                    `If in doubt, you can use the \`${pkg} commit\``,
+                );
+                assert.fileContent(
+                    "CONTRIBUTING.md",
+                    `Be sure to run \`${pkg} test\``,
+                );
+            });
 
-        it("should always pass", () => {
-            expect(1).toEqual(1);
-        });
-    });
+            it("should create the correct CHANGELOG.md", () => {
+                assert.fileContent(
+                    "CHANGELOG.md",
+                    `All notable changes to the "${name}" project will be documented in this file.`,
+                );
+            });
+
+            it("should create the correct .all-contributorsrc", () => {
+                assert.fileContent(
+                    ".all-contributorsrc",
+                    `"projectName": "${name}"`,
+                );
+            });
+
+            it("should create the correct husky git hooks", () => {
+                assert.fileContent(
+                    ".husky/commit-msg",
+                    `${pkg} commitlint --edit $1`,
+                );
+                assert.fileContent(".husky/pre-commit", `${pkg} lint-staged`);
+            });
+
+            it("should create a git repository if git is true", () => {
+                git ? assert.file(".git") : assert.noFile(".git");
+            });
+
+            it("should always pass", () => {
+                expect(1).toEqual(1);
+            });
+        },
+    );
 });

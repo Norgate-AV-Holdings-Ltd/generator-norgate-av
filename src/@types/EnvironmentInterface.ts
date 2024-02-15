b92c27e3fe3b628d2e58203618d52cdd823b544d
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-02-15 22:51:30 +0000

    refactor: further updates

--- src/@types/EnvironmentInterface.ts
+++ src/@types/EnvironmentInterface.ts
@@ -1,4 +1,7 @@
 export interface EnvironmentInterface {
     initialize(): Promise<void>;
+    getStore(): { [key: string]: unknown };
+    getDependency(): string;
+    getDevDependency(): string;
     getEngine(): string;
 }

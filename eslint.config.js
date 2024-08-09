2c5d8f47e332758e4b9da41a85ab19fb4135ad5b
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-08-09 16:29:44 +0100

    chore(eslint): update eslint config to new flat version

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    prettierConfig,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": [
+                "warn",
+                { ignoreRestArgs: true },
+            ],
+            "@typescript-eslint/no-unused-expressions": "warn",
+        },
+    },
+    {
+        ignores: [
+            "build/*",
+            "dist/*",
+            "node_modules/*",
+            ".history/*",
+            "templates/*",
+            "eslint.config.js",
+        ],
+    },
+);
